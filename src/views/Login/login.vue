79bf752995a9fa480bbb5d6040ef65350d6e9cdf
liuteng <t13005455706@163.com>
2022-06-27 13:44:31 +0800

    构建项目 配置全局样式、路由、element-ui

---
+++ src/views/Login/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="login">
+    <h1>登录11111111111111111111111111</h1>
+    <el-button type="primary">测试</el-button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 50vh;
+  background-color: aqua;
+}
+</style>
